f2f36067eac12cc8e067c089f54689f21fac55bd
Mario Alva <magrio.ag@gmail.com>
2012-05-21 02:19:17 +0000

    Starting with empty plugin

---
+++ wp-secure-categories.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Secure Categories
+Plugin URI: http://secure-categories.chinga.us/
+Description: Now you can protect categories with password muahahaha
+Version: 0.0.1
+Author: Mario Alvarez
+Author URI: http://marioalva.info/
+License: WTFPL :p
+*/
